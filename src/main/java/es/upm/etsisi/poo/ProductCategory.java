9c3af59a776495e79daf228166dbfe5df034ef75
Wincynin <1234ninunauty@gmail.com>
2025-09-23 19:04:07 +0200

    Added javadoc and normal comments to Product.java, Catalog.java and ProductCategory.java enum

--- src/main/java/es/upm/etsisi/poo/ProductCategory.java
+++ src/main/java/es/upm/etsisi/poo/ProductCategory.java
@@ -1,5 +1,17 @@
 package es.upm.etsisi.poo;
 
+/**
+ * Enum representing the different categories of products available.
+ * Each category is associated with a specific discount rate.
+ *
+ * <ul>
+ *   <li>{@link #MERCH} - Merchandise with no discount.</li>
+ *   <li>{@link #PAPELERIA} - Stationery with a 5% discount.</li>
+ *   <li>{@link #ROPA} - Clothing with a 7% discount.</li>
+ *   <li>{@link #LIBRO} - Books with a 10% discount.</li>
+ *   <li>{@link #ELECTRONICA} - Electronics with a 3% discount.</li>
+ * </ul>
+ */
 public enum ProductCategory {
     MERCH(0.0),
     PAPELERIA(0.05),
