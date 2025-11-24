013192c3c6057173d1d256147c3a9941d4c39b7a
wincynin <1234ninunauty@gmail.com>
2025-11-24 20:59:28 +0100

    Refactor EventProduct constructor to remove maxParticipants parameter and enforce a constant maximum. Simplify validation logic by setting maxParticipants directly within the constructor.

--- src/main/java/es/upm/etsisi/poo/domain/product/EventProduct.java
+++ src/main/java/es/upm/etsisi/poo/domain/product/EventProduct.java
@@ -4,18 +4,14 @@ import java.time.LocalDateTime;
 
 public class EventProduct extends BookableProduct {
     private static final int MAX_PARTICIPANTS = 100;
-
     private final int maxParticipants;
     private final LocalDateTime expirationDate;
     private final EventType eventType;
 
-    public EventProduct(String name, double price, LocalDateTime expirationDate, int maxParticipants, EventType eventType) {
+    public EventProduct(String name, double price, LocalDateTime expirationDate, EventType eventType) {
         super(name, price);
-        if (maxParticipants > MAX_PARTICIPANTS) {
-            throw new IllegalArgumentException("Error: Maximum number of participants cannot exceed " + MAX_PARTICIPANTS);
-        }
         this.expirationDate = expirationDate;
-        this.maxParticipants = maxParticipants;
+        this.maxParticipants = MAX_PARTICIPANTS;
         this.eventType = eventType;
     }
 
